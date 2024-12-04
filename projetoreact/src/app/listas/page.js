67c33df11421a97f7b7d1b221f87c3a2658a261e
fernandaoooo <mariafernandamenezes002@gmail.com>
2024-12-04 08:16:47 -0400

    04/12

--- projetoreact/src/app/listas/page.js
+++ projetoreact/src/app/listas/page.js
@@ -1,7 +1,46 @@
 export default function Listas() {
+    const racas = ['Vira-lata', 'Pit-bull', 'Pug', 'Shitzu', 'Labrador']
+
+    const racasObj = [
+        {
+            id:1,
+            raca: 'Vira-lata',
+        },
+        {
+            id:2,
+            raca: 'Pit-bull',
+        },
+        {
+            id:3,
+            raca: 'Pug',
+        },
+        {
+            id:4,
+            raca: 'Shitzu',
+        },
+        {
+            id:5,
+            raca: 'Labrador',
+        }
+    ]
+
     return (
-        <div>
-            <h1>Listas</h1>
+        <div style={{display: 'flex', flexDirection: 'column', textAlign: 'center'}}>
+            <h1>Lista comum</h1>
+            <ul>
+                {racas.map((raca,i) => (
+                    <li key={i}>{i+1} - {raca}</li>
+                ))}
+            </ul>
+            <h2>Lista de objetos</h2>
+            <ul>
+                {racasObj.map((racaObj) => (
+                    <div key={racaObj.id}>
+                        <h3>{racaObj.id}</h3>
+                        <p>{racaObj.raca}</p>
+                    </div>
+                ))}
+            </ul>
         </div>
     )
 }
