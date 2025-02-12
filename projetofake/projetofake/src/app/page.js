5db5f1b44def689b4d9ff56d8d603aa64a8e867f
fernandaoooo <mariafernandamenezes002@gmail.com>
2025-02-12 08:02:01 -0400

    12/02

--- projetofake/projetofake/src/app/page.js
+++ projetofake/projetofake/src/app/page.js
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Image src="/images/pacientefeliz.jpg" alt="Logo" width={700} height={450}/>
+      <p className={styles.title}>Viva Menos, mas com mais qualidade de vida.💀🩸</p>
+      <Image src="/images/pacientefeliz.jpg" alt="Logo" width={500} height={330}/>
       <div className={styles.frase}>
-        <p>Viva Menos, mas com mais qualidade de vida.💀🩸
-          <br/>
+        <p className={styles.mensagem}>
           Aqui, reduzimos o excesso e focamos no essencial. Menos complicação, mais saúde. Nosso trabalho é garantir que você viva o melhor, sem exageros. Afinal, viver bem não precisa ser difícil, nem cheio de promessas vazias.
         </p>
       </div>
